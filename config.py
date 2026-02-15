fb609ee6fad34859281c40ec716b2bb0b3c44bba
Mathisse Meynen <150125526+Mathissemeynen@users.noreply.github.com>
2026-02-15 22:59:58 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 # config.py for Bluesky
 
 # Your Bluesky handle (e.g., "username.bsky.social")
-BLUESKY_HANDLE = "mmeynen.bsky.social"
+BLUESKY_HANDLE = "" 
 
 # The app password you just generated (not your login password)
-BLUESKY_APP_PASSWORD = "WtfWrmMoetDees"
+
+BLUESKY_APP_PASSWORD = ""
+
+# we need this so you don't accidentally push your credentials to github :)
