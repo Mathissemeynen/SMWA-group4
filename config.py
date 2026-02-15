5bcb6de5ac5dde7b0e55f4ad077e9cf146e235f1
Mathisse Meynen <150125526+Mathissemeynen@users.noreply.github.com>
2026-02-15 22:57:33 +0100

    first lecture

---
+++ config.py
@@ -0,0 +1,7 @@
+# config.py for Bluesky
+
+# Your Bluesky handle (e.g., "username.bsky.social")
+BLUESKY_HANDLE = "mmeynen.bsky.social"
+
+# The app password you just generated (not your login password)
+BLUESKY_APP_PASSWORD = "WtfWrmMoetDees"
